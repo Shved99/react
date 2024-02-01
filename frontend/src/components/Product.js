e79b69086f9ac56d6c16815b028e7a2dd0e7faae
zerodbee <super7799@mail.ru>
2024-02-01 18:14:10 +0300

    commit

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './Product.css';
+
+
+function Product({ id, image, title, price, setBasket, setBasketPrice, setBasketQty, basket, setMessage, setModalBox, token }) {
+
+  const product = {
+    id: id,
+    image: image,
+    title: title,
+    price: price
+  }
+
+  function addToBasket() {
+    const index = basket.findIndex(value => value.id === product.id)
+    // console.debug(index)
+
+    if (index === -1) {
+      setBasket(prevState => [...prevState, product])
+      setBasketPrice(current => current + product.price)
+      setBasketQty(current => current + 1)
+    } else {
+      return
+    }
+
+    setTimeout(() => {
+      setMessage('Товар добавлен в корзину.')
+      setModalBox('MessageBox')
+    }, 100)
+  }
+
+  function AddToBasketButton() {
+    if (token !== null) {
+      return (
+        <>
+          <button className='buy' onClick={() => addToBasket()}>В корзину</button>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <p>Требуется авторизация</p>
+        </>
+      )
+    }
+  }
+
+  return (
+    <div className="Product">
+      
+      <img width="200px" src="https://cdn.nonton.ru/resize_cache/2430800/9b198b03fb7fb87137cfd873d1b3a69c/iblock/c0f/DSC04341.jpeg" alt='Изображение товара' />
+      <h1>{product.title}</h1>
+      <p>{product.price} рублей</p>
+      <AddToBasketButton />
+
+    </div>
+  );
+}
+
+export default Product;
---
+++ frontend/src/components/ProductBasket.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './ProductBasket.css';
+
+function ProductBasket({ id, image, title, price, setBasket, setBasketPrice, setBasketQty }) {
+
+  const [qty, setQty] = useState(1)
+
+  function deleteBasket() {
+    setBasket(current => current.filter(product => product.id !== id))
+    setBasketPrice(current => current - (price * qty))
+    setBasketQty(current => current - qty)
+  }
+
+  function plus() {
+    setQty(current => current + 1)
+    setBasketPrice(current => current + price)
+    setBasketQty(current => current + 1)
+  }
+
+  function minus() {
+    if (qty > 1) {
+      setQty(current => current - 1)
+      setBasketPrice(current => current - price)
+      setBasketQty(current => current - 1)
+    }
+  }
+
+  return (
+    <div className="ProductBasket">
+<img width="200px" src="https://cdn.nonton.ru/resize_cache/2430800/9b198b03fb7fb87137cfd873d1b3a69c/iblock/c0f/DSC04341.jpeg" alt='Изображение товара' />
+      <h1>{title}</h1>
+      <p>{price} рублей</p>
+      
+      <div className='basketQty'>
+        <button className='minus' onClick={() => minus()}>-</button>
+        <p className='qty'>{qty}</p>
+        <button className='plus' onClick={() => plus()}>+</button>
+      </div>
+      <button className='del' onClick={() => deleteBasket()}>Удалить</button>
+    </div>
+  );
+}
+
+export default ProductBasket;
